228092a8353c3f2e84e3ab60cf08b32467a5a49d
j3ssie <jeromej3m@gmail.com>
2020-04-15 16:49:28 +0700

    Add small tools for strip out similar URLs

---
+++ durl/README.md
@@ -0,0 +1,14 @@
+## Diff URLs
+
+Strip out similar URLs by unique hostname-path-paramName
+
+## Install
+```
+go get -u github.com/j3ssie/go-auxs/durl
+```
+
+## Usage
+```
+cat wayback_urls.txt | durl | tee differ_urls.txt
+```
+
---
+++ durl/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha1"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+)
+
+// diff URLs
+// Strip out similar URLs by unique hostname-path-paramName
+
+
+func main() {
+	data := make(map[string]string)
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		raw := strings.TrimSpace(sc.Text())
+		if sc.Err() != nil && raw == "" {
+			continue
+		}
+		hash := hashUrl(raw)
+		if hash == "" {
+			continue
+		}
+		_, exist := data[hash]
+		if !exist {
+			data[hash] = raw
+			fmt.Println(raw)
+		}
+	}
+}
+
+// hashUrl gen unique hash base on url
+func hashUrl(raw string) string {
+	u, err := url.Parse(raw)
+	if err != nil {
+		return ""
+	}
+
+	var queries string
+	for k := range u.Query() {
+		queries += fmt.Sprintf("%v", k)
+	}
+
+	data := fmt.Sprintf("%v-%v-%v", u.Hostname(), u.Path, queries)
+	return genHash(data)
+}
+
+// genHash gen SHA1 hash from string
+func genHash(text string) string {
+	h := sha1.New()
+	h.Write([]byte(text))
+	hashed := h.Sum(nil)
+	return fmt.Sprintf("%x", hashed)
+}
